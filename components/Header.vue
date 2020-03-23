209a18c3bf5af5b0d2b6091457cf324a12cb9a95
Sarmat Khachirov <skhachirov12@gmail.com>
2020-03-23 17:57:39 +0300

    Header comp

---
+++ components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <header class="header">
+    <nav class="inner">
+      <h1 class="header__title">News Explorer</h1>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.header {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: $color-primary;
+
+  &__title {
+    font-size: 2rem;
+    font-weight: 300;
+  }
+
+  .inner {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 5rem;
+    max-width: 80rem;
+  }
+}
+</style>
