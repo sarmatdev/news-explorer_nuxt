e4f74e38548701ac9bfefd7bb368c80b309800d4
Sarmat Khachirov <skhachirov12@gmail.com>
2020-03-24 17:02:43 +0300

    install fonawesome

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,38 +1,41 @@
 export default {
-  mode: "universal",
+  mode: 'universal',
   /*
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || "",
+    title: process.env.npm_package_name || '',
     meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
-        hid: "description",
-        name: "description",
-        content: process.env.npm_package_description || ""
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || ''
       }
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
   /*
    ** Customize the progress-bar color
    */
-  loading: { color: "#fff" },
+  loading: { color: '#00bfa6' },
   /*
    ** Global CSS
    */
-  css: ["~/assets/scss/main.scss"],
+  css: [
+    '~/assets/scss/main.scss',
+    '@fortawesome/fontawesome-svg-core/styles.css'
+  ],
   styleResources: {
     scss: [
-      "~/assets/scss/_variables.scss" // use underscore "_" & also file extension ".scss"
+      '~/assets/scss/_variables.scss' // use underscore "_" & also file extension ".scss"
     ]
   },
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [],
+  plugins: ['~/plugins/fontawesome.js'],
   /*
    ** Nuxt.js dev-modules
    */
@@ -42,10 +45,10 @@ export default {
    */
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
-    "@nuxtjs/axios",
+    '@nuxtjs/axios',
     // Doc: https://github.com/nuxt-community/dotenv-module
-    "@nuxtjs/dotenv",
-    "@nuxtjs/style-resources"
+    '@nuxtjs/dotenv',
+    '@nuxtjs/style-resources'
   ],
   /*
    ** Axios module configuration
