57797fb35edeee26f1536ccc76d990d078337f07
Sarmat Khachirov <skhachirov12@gmail.com>
2020-03-25 12:37:22 +0300

    Render news

---
+++ components/NewsItem.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="news-card">
+    <h1 class="news-card__title">
+      <a :href="news.url" target="_blank">
+        {{ news.title }}
+      </a>
+      <span>({{ news.source.name }})</span>
+    </h1>
+    <p class="news-card__published">Published {{ news.published }} ago</p>
+    <!-- <font-awesome-icon
+      class="news-card__icon"
+      :class="{ rotate: active }"
+      @click="expand"
+      :icon="['fas', 'chevron-down']"
+    /> -->
+    <!-- <article class="news-card__description" :class="{ active }">
+      {{ news.description }}
+    </article> -->
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['news'],
+  data() {
+    return {
+      active: false
+    };
+  },
+  methods: {
+    expand() {
+      this.active = !this.active;
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.news-card {
+  min-height: 8rem;
+  height: 100%;
+  max-width: 80rem;
+  width: 100%;
+  border-radius: 0.5rem;
+  background-color: $color-white;
+  padding: 2rem;
+  margin-bottom: 0.5rem;
+
+  &:last-child {
+    margin-bottom: 0;
+  }
+
+  &__published {
+    font-size: 1.4rem;
+    color: $color-grey;
+  }
+
+  &__description {
+    max-height: 0;
+    overflow: hidden;
+    transition: all 0.3s ease;
+  }
+
+  &__icon {
+    cursor: pointer;
+    transition: transform 0.3s ease;
+  }
+  .rotate {
+    transform: rotate(180deg);
+  }
+
+  &__title {
+    font-size: 1.4rem;
+    font-weight: 400;
+    color: $color-secondary;
+
+    span {
+      color: $color-primary;
+    }
+
+    a {
+      text-decoration: none;
+      color: $color-secondary;
+
+      &:visited {
+        color: $color-secondary;
+      }
+    }
+  }
+}
+.active {
+  max-height: 100%;
+}
+</style>
---
+++ components/NewsList.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="news-grid">
+    <NewsItem v-for="(news, id) in news" :key="id" :news="news" />
+  </section>
+</template>
+
+<script>
+import NewsItem from '@/components/NewsItem';
+export default {
+  components: {
+    NewsItem
+  },
+  computed: {
+    news() {
+      return this.$store.getters.news;
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.news-grid {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 2rem 0;
+  // display: grid;
+  // grid-template-columns: repeat(auto-fit, minmax(10rem, 25rem));
+  // justify-content: space-evenly;
+  // align-content: space-between;
+  // align-items: start;
+  // padding: 4rem 0;
+  // grid-gap: 2rem 1rem;
+}
+</style>
