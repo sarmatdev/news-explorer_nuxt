c5d6626313f4b272793edb6a5754384badfc3e6d
Sarmat Khachirov <skhachirov12@gmail.com>
2020-03-25 12:38:21 +0300

    Render news

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,18 @@
-<template> </template>
+<template>
+  <NewsList />
+</template>
 
 <script>
-export default {};
+import NewsList from "@/components/NewsList";
+
+export default {
+  components: {
+    NewsList
+  },
+  async fetch({ store }) {
+    await store.dispatch("getNews");
+  }
+};
 </script>
 
 <style></style>
