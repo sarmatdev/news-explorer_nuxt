6ca77e31f010bdaa0f019a963eb841ebb9eb8984
Sarmat Khachirov <skhachirov12@gmail.com>
2020-03-26 10:10:46 +0300

    Create categories page

---
+++ pages/_categories/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <NewsList />
+</template>
+
+<script>
+import NewsList from '@/components/NewsList';
+
+export default {
+  components: { NewsList },
+  mounted() {
+    console.log(this.$route.params.categories);
+  },
+
+  async fetch({ store, params }) {
+    await store.dispatch('getNewsByСategory', params.categories);
+  }
+};
+</script>
+
+<style></style>
