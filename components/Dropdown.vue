9e0343fdad76f6eb73b6122f68b0737cc06a026e
Sarmat Khachirov <skhachirov12@gmail.com>
2020-03-26 10:09:43 +0300

    Rebuild news array & add publish time

---
+++ components/Dropdown.vue
@@ -0,0 +1,60 @@
+<template>
+  <li class="menu__item" @click="isOpen = !isOpen">
+    Categories
+    <span>
+      <font-awesome-icon :icon="['fas', 'angle-down']" />
+    </span>
+    <div class="sub-menu" v-if="isOpen">
+      <nuxt-link
+        class="sub-menu__link"
+        v-for="(category, i) in categories"
+        :key="i"
+        :to="category.link"
+        >{{ category.title }}</nuxt-link
+      >
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'dropdown',
+  data() {
+    return {
+      isOpen: false,
+      categories: [
+        {
+          title: 'Business',
+          link: '/business'
+        },
+        {
+          title: 'Entertainment',
+          link: '/entertainment'
+        },
+        {
+          title: 'General',
+          link: '/general'
+        },
+        {
+          title: 'Health',
+          link: '/health'
+        },
+        {
+          title: 'Science',
+          link: '/science'
+        },
+        {
+          title: 'Sports',
+          link: '/sports'
+        },
+        {
+          title: 'Technology',
+          link: '/technology'
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style></style>
