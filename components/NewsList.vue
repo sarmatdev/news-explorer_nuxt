f320f6709441030d43c4c06e11583b1b97759d9c
Sarmat Khachirov <skhachirov12@gmail.com>
2020-03-26 14:55:53 +0300

    Add search error

--- components/NewsList.vue
+++ components/NewsList.vue
@@ -1,5 +1,14 @@
 <template>
   <section class="news-grid">
+    <p class="news-grid__title">
+      {{ title }}
+    </p>
+    <img
+      v-if="notFound"
+      class="news-grid__not-found"
+      src="/img/not-found.svg"
+      alt=""
+    />
     <NewsItem v-for="(news, id) in news" :key="id" :news="news" />
   </section>
 </template>
@@ -13,6 +22,20 @@ export default {
   computed: {
     news() {
       return this.$store.getters.news;
+    },
+    title() {
+      if (this.$route.params.categories) {
+        return `${this.$route.params.categories} news`;
+      } else if (this.$route.params.query) {
+        return `News by "${this.$route.params.query}"`;
+      }
+      return 'Top Headlines';
+    },
+    notFound() {
+      if (this.news.length === 0) {
+        return true;
+      }
+      return false;
     }
   }
 };
@@ -24,12 +47,22 @@ export default {
   flex-direction: column;
   align-items: center;
   margin: 2rem 0;
-  // display: grid;
-  // grid-template-columns: repeat(auto-fit, minmax(10rem, 25rem));
-  // justify-content: space-evenly;
-  // align-content: space-between;
-  // align-items: start;
-  // padding: 4rem 0;
-  // grid-gap: 2rem 1rem;
+
+  &__title {
+    font-size: 2rem;
+    text-transform: uppercase;
+    color: $color-secondary;
+    background-color: $color-primary;
+    padding: 1rem;
+    transform: skewY(-2deg);
+    margin-bottom: 1rem;
+  }
+
+  &__not-found {
+    max-width: 80rem;
+    height: 100%;
+    width: 100%;
+    object-fit: contain;
+  }
 }
 </style>
