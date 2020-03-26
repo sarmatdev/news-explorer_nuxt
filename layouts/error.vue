4f2ef6125139984c2618f76b4921d9d83ada7da4
Sarmat Khachirov <skhachirov12@gmail.com>
2020-03-26 14:56:36 +0300

    Create error page

---
+++ layouts/error.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="error">
+    <img class="error__img" src="/img/404.svg" alt="" />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.error {
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+
+  &__img {
+    max-width: 80rem;
+    height: 100%;
+    width: 100%;
+    object-fit: contain;
+  }
+}
+</style>
