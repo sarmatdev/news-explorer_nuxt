d68ee0716938d932355297311e5b35d1c64d377b
Sarmat Khachirov <skhachirov12@gmail.com>
2020-03-26 14:55:09 +0300

    Create search page

---
+++ pages/search/_query.vue
@@ -0,0 +1,18 @@
+<template>
+  <NewsList />
+</template>
+
+<script>
+import NewsList from '@/components/NewsList';
+
+export default {
+  components: {
+    NewsList
+  },
+  async fetch({ store, params }) {
+    await store.dispatch('searchNews', params.query);
+  }
+};
+</script>
+
+<style></style>
