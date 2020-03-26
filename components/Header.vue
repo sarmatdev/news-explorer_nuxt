c2f3080c189dbb4179845184e0f07bd202ce1fd3
Sarmat Khachirov <skhachirov12@gmail.com>
2020-03-26 10:10:20 +0300

    Add navigation

--- components/Header.vue
+++ components/Header.vue
@@ -1,13 +1,24 @@
 <template>
   <header class="header">
-    <nav class="inner">
-      <h1 class="header__title">News Explorer</h1>
+    <h1 class="header__title">News Explorer</h1>
+    <nav>
+      <ul class="menu">
+        <nuxt-link tag="li" class="menu__item" to="/">
+          Top Headlines
+        </nuxt-link>
+        <Dropdown />
+      </ul>
     </nav>
   </header>
 </template>
 
 <script>
-export default {};
+import Dropdown from '@/components/Dropdown';
+export default {
+  components: {
+    Dropdown
+  }
+};
 </script>
 
 <style lang="scss">
@@ -15,21 +26,53 @@ export default {};
   display: flex;
   align-items: center;
   justify-content: center;
-  // background-color: $color-primary;
   background-color: $color-secondary;
+  height: 5rem;
+  position: relative;
 
   &__title {
-    font-size: 2rem;
+    font-size: 1.6rem;
     font-weight: 300;
     color: $color-white;
   }
+}
 
-  .inner {
+.menu {
+  margin-left: 2rem;
+  display: flex;
+  align-items: center;
+
+  &__item {
+    text-decoration: none;
+    cursor: pointer;
+    color: $color-white;
+    list-style: none;
+    margin-right: 1rem;
+    transition: all 0.2s ease;
+
+    &:hover {
+      color: $color-primary;
+    }
+  }
+
+  .sub-menu {
+    position: absolute;
+    background-color: $color-primary;
+    padding: 1rem;
+    border-radius: 0.5rem;
     display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 5rem;
-    max-width: 80rem;
+    flex-direction: column;
+
+    &__link {
+      transition: all 0.2s ease;
+      color: $color-white;
+      text-decoration: none;
+      margin-bottom: 0.5rem;
+
+      &:hover {
+        color: $color-secondary;
+      }
+    }
   }
 }
 </style>
